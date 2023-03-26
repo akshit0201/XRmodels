8522c40d18f7769ad3b6a0cbc6dbd03612be0ff7
akshit0201 <57068848+akshit0201@users.noreply.github.com>
2023-03-26 18:17:36 +0000

    added port env

--- src/server/server.ts
+++ src/server/server.ts
@@ -2,7 +2,7 @@ import express from "express";
 import path from "path";
 import http from "http";
 
-const port: number = 3000;
+const port: any = process.env.PORT || 3000;
 
 class App {
 	private server: http.Server;
